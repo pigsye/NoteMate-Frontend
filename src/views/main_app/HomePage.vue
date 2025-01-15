673c0df45e50f62fa91dac093d6ea6d66d3dfcac
pigsye <eayancheong@gmail.com>
2025-01-15 17:29:01 +0800

    uh
    uhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/views/main_app/HomePage.vue
+++ src/views/main_app/HomePage.vue
@@ -7,9 +7,7 @@
       </div>
     </div>
     <div class="right-side">
-      <!-- Dynamically rendering the component based on `currentComponent` -->
-      <component :is="NotesEditor" v-if="currentComponent" :info="apiInfo.component?.componentInfo" />
-      <h1 style="color:white">{{ apiInfo }}, {{ apiInfo.component?.componentname }}</h1>
+      <NotesEditor :info="info" />
     </div>
   </div>
 </template>
@@ -27,51 +25,15 @@ export default {
           selected: ''
         }
       },
-      currentComponent: 'NotesEditor', // For storing the dynamically imported component
-      apiInfo: {} // For storing the API response data
+      info: {
+        topic: 'Cells',
+        notesContent: '<p> meow </p>'
+      }
     }
   },
   components: {
     SpacesComponent,
     NotesEditor: () => import('@/components/main_app/NotesEditor.vue')
-  },
-  mounted () {
-    this.loadComponent()
-  },
-  methods: {
-    async loadComponent () {
-      try {
-        // Fetch data from the API to determine which component to render
-        const response = await fetch('/api/data')
-        const data = await response.json()
-
-        // Log the component name for debugging
-        console.log('Component to load:', data.component.componentname)
-
-        this.apiInfo = data
-
-        // Dynamically import the correct component
-        switch (data.component?.componentname) {
-          case 'NotesEditor':
-            this.currentComponent = () => import('@/components/main_app/NotesEditor.vue')
-            console.log('bamewo')
-            break
-          case 'ChooseSubject':
-            this.currentComponent = () => import('@/components/main_app/ChooseSubject.vue')
-            console.log('owem')
-            break
-          case 'ChooseNotes':
-            this.currentComponent = () => import('@/components/main_app/ChooseNotes.vue')
-            console.log('mewo2')
-            break
-          default:
-            console.error('Unknown component:', data.component.componentname)
-            this.currentComponent = null // Fallback in case of an error
-        }
-      } catch (error) {
-        console.error('Error loading component:', error)
-      }
-    }
   }
 }
 </script>
